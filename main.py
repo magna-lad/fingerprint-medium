3c819c2ef5d9da14e8231373901dfa419ec5c093
Sangam Koundal <koundalsangam314@gmail.com>
2025-07-22 17:45:12 +0530

    gabor filters, minutiae extraction, skeletonization

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-import random
 import numpy as np
 import cv2
 import torch
@@ -12,9 +10,6 @@ import pandas as pd
 from scipy import stats
 from scipy.interpolate import interp1d
 from skimage.morphology import skeletonize as skelt
-import warnings
-from PIL import Image
-from scipy.stats import pearsonr
 # takes in the img as the object
 
 # returns an object containing the minutias detail of a fingerprint 
@@ -32,7 +27,7 @@ class minutiaLoader:  # will only handle loading of the minutia, segmentation
         self.normalised_img= self.normalise(self.img)
         self.segmented_img, self.norm_img, self.mask = self.segmentation(self.normalised_img)
 
-        extractor = minutiaExtractor(self.normalised_img,self.segmented_img,self.norm_img,self.mask,self.block)
+        
 
     def load(self):
         img = cv2.imread(self.img_path,0) # image is already loaded in grayscale
@@ -68,7 +63,7 @@ class minutiaLoader:  # will only handle loading of the minutia, segmentation
         mask[image_variance < threshold] = 0
 
         # smooth mask with a open/close morphological filter
-        kernel_size = min(self.block * 1.5, w // 5)  # Cap kernel size
+        kernel_size = min(self.block * 2, w // 4)  # Cap kernel size
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_size, kernel_size))
     
         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
@@ -83,6 +78,9 @@ class minutiaLoader:  # will only handle loading of the minutia, segmentation
         norm_img = (im - mean_val)/(std_val)
     
         return segmented_img, norm_img, mask
+    
+    
+
 
 
 # will output the x,y,type and angle of the point minutia
@@ -92,13 +90,17 @@ class minutiaExtractor:
         self.y_cord = None
         self.angle_minutia = None
         self.type = None
+
+
         self.block = block
-        
         self.normalised_img = normalised_img
         self.segmented_img=segmented_img
         self.norm_img=norm_img
         self.mask = mask
-        self.angle_gabor=[]
+        self.angle_gabor=[] # required for gabor filters
+        self.freq=[]
+        self.gabor_img=None
+        self.skeleton = None
 
     # for gabor filters
     def angleCalculation(self,smooth=False):
@@ -172,6 +174,37 @@ class minutiaExtractor:
 
         return smooth_angles    
     
+
+    # just to visualise the angles
+    def get_line_ends(self,i, j, tang):
+        if -1 <= tang and tang <= 1:
+            begin = (i, int((-self.block/2) * tang + j + self.block/2))
+            end = (i + self.block, int((self.block/2) * tang + j + self.block/2))
+        else:
+            begin = (int(i + self.block/2 + self.block/(2 * tang)), j + self.block//2)
+            end = (int(i + self.block/2 - self.block/(2 * tang)), j - self.block//2)
+        return (begin, end)
+
+    # segmented_img-im
+    # mask-mask
+    # angle- angle_gabor
+    # w- self.block
+    def visualize_angles(self):
+        (y, x) = self.segmented_img.shape
+        result = cv2.cvtColor(np.zeros(self.segmented_img.shape, np.uint8), cv2.COLOR_GRAY2RGB)
+        mask_threshold = (self.segmented_img-1)**2
+        for i in range(1, x, self.segmented_img):
+            for j in range(1, y, self.block):
+                radian = np.sum(self.mask[j - 1:j + self.block, i-1:i+self.block])
+                if radian > mask_threshold:
+                    tang = math.tan(self.angle_gabor[(j - 1) // self.block][(i - 1) // W])
+                    (begin, end) = minutiaExtractor.get_line_ends(i, j, self.block, tang)
+                    cv2.line(result, begin, end, color=150)
+
+        cv2.resize(result, self.segmented_img.shape, result)
+        return result
+    
+
                 #self.normim,sel.mask,self.angle_gabor,self.block
     #freq = ridge_freq(normim, mask, angles, block_size, kernel_size=5, minWaveLength=5, maxWaveLength=15)
     def ridge_freq(self,kernel_size=5, minWaveLength=5, maxWaveLength=15):
@@ -180,12 +213,12 @@ class minutiaExtractor:
         rows,cols = self.norm_img.shape
         freq = np.zeros((rows,cols))
 
-        for row in range(0, rows - self.block_size, self.block_size):
-            for col in range(0, cols - self.block_size, self.block_size):
-                image_block = self.norm_img[row:row + self.block_size][:, col:col + self.block_size]
-                angle_block = self.angle_gabor[row // self.block_size][col // self.block_size]
+        for row in range(0, rows - self.block, self.block):
+            for col in range(0, cols - self.block, self.block):
+                image_block = self.norm_img[row:row + self.block][:, col:col + self.block]
+                angle_block = self.angle_gabor[row // self.block][col // self.block]
                 if angle_block:
-                    freq[row:row + self.block_size][:, col:col + self.block_size] = minutiaExtractor.frequest(image_block, angle_block, kernel_size,minWaveLength, maxWaveLength)
+                    freq[row:row + self.block][:, col:col + self.block] = minutiaExtractor.frequest(image_block, angle_block, kernel_size,minWaveLength, maxWaveLength)
 
         freq = freq*self.mask
         freq_1d = np.reshape(freq,(1,rows*cols))
@@ -194,7 +227,7 @@ class minutiaExtractor:
         ind = ind[1,:]
         non_zero_elems_in_freq = freq_1d[0][ind]
         medianfreq = np.median(non_zero_elems_in_freq) * self.mask
-
+        self.freq=medianfreq
         return medianfreq
     
     @staticmethod
@@ -242,13 +275,164 @@ class minutiaExtractor:
             else:
                 freq_block = np.zeros(im.shape)
         return(freq_block)
+
+                    #norm_img, angle_gabor, freq
+    def gabor_filter(self,kx=0.65, ky=0.65):
+        """
+        Gabor filter is a linear filter used for edge detection. Gabor filter can be viewed as a sinusoidal plane of
+        particular frequency and orientation, modulated by a Gaussian envelope.
+        :param im:
+        :param orient:
+        :param freq:
+        :param kx:
+        :param ky:
+        :return:
+        """
+        angleInc = 3
+        im = np.double(self.norm_img)
+        rows, cols = im.shape
+        return_img = np.zeros((rows,cols))
+
+        # Round the array of frequencies to the nearest 0.01 to reduce the
+        # number of distinct frequencies we have to deal with.
+        freq_1d = self.freq.flatten()
+        frequency_ind = np.array(np.where(freq_1d>0))
+        non_zero_elems_in_freq = freq_1d[frequency_ind]
+        non_zero_elems_in_freq = np.double(np.round((non_zero_elems_in_freq*100)))/100
+        unfreq = np.unique(non_zero_elems_in_freq)
+
+        # Generate filters corresponding to these distinct frequencies and
+        # orientations in 'angleInc' increments.
+        sigma_x = 1/unfreq*kx
+        sigma_y = 1/unfreq*ky
+        block_size = np.round(3*np.max([sigma_x,sigma_y]))
+        block_size = int(block_size)
+        array = np.linspace(-block_size,block_size,(2*block_size + 1))
+        x, y = np.meshgrid(array, array)
+
+        # gabor filter equation
+        reffilter = np.exp(-(((np.power(x,2))/(sigma_x*sigma_x) + (np.power(y,2))/(sigma_y*sigma_y)))) * np.cos(2*np.pi*unfreq[0]*x)
+        filt_rows, filt_cols = reffilter.shape
+        gabor_filter = np.array(np.zeros((180//angleInc, filt_rows, filt_cols)))
+
+        # Generate rotated versions of the filter.
+        for degree in range(0,180//angleInc):
+            rot_filt = scipy.ndimage.rotate(reffilter,-(degree*angleInc + 90),reshape = False)
+            gabor_filter[degree] = rot_filt
+
+        # Convert orientation matrix values from radians to an index value that corresponds to round(degrees/angleInc)
+        maxorientindex = np.round(180/angleInc)
+        orientindex = np.round(self.angle_gabor/np.pi*180/angleInc)
+        for i in range(0,rows//16):
+            for j in range(0,cols//16):
+                if(orientindex[i][j] < 1):
+                    orientindex[i][j] = orientindex[i][j] + maxorientindex
+                if(orientindex[i][j] > maxorientindex):
+                    orientindex[i][j] = orientindex[i][j] - maxorientindex
+
+        # Find indices of matrix points greater than maxsze from the image boundary
+        block_size = int(block_size)
+        valid_row, valid_col = np.where(self.freq>0)
+        finalind = \
+            np.where((valid_row>block_size) & (valid_row<rows - block_size) & (valid_col>block_size) & (valid_col<cols - block_size))
+
+        for k in range(0, np.shape(finalind)[1]):
+            r = valid_row[finalind[0][k]]; c = valid_col[finalind[0][k]]
+            img_block = im[r-block_size:r+block_size + 1][:,c-block_size:c+block_size + 1]
+            return_img[r][c] = np.sum(img_block * gabor_filter[int(orientindex[r//16][c//16]) - 1])
+
+        gabor_img = 255 - np.array((return_img < 0)*255).astype(np.uint8)
+        self.gabor_img = gabor_img
+        return gabor_img
+    
+    def skeletonize(self):
+        """
+        https://scikit-image.org/docs/dev/auto_examples/edges/plot_skeleton.html
+        Skeletonization reduces binary objects to 1 pixel wide representations.
+        skeletonize works by making successive passes of the image. On each pass, border pixels are identified
+        and removed on the condition that they do not break the connectivity of the corresponding object.
+        :param image_input: 2d array uint8
+        :return:
+        """
+        image = np.zeros_like(self.gabor_img)
+        image[self.gabor_img == 0] = 1.0
+        output = np.zeros_like(self.gabor_img)
+
+        skeleton = skelt(image)
+
+        output[skeleton] = 255
+        cv2.bitwise_not(output, output)
+        self.skeleton = output
+        return output
+
+    def calculate_minutiaes(self, kernel_size=3):
+        biniry_image = np.zeros_like(self.skeleton)
+        biniry_image[self.skeleton<10] = 1.0
+        biniry_image = biniry_image.astype(np.int8)
+
+        (y, x) = self.skeleton.shape
+        result = cv2.cvtColor(self.gabor_img, cv2.COLOR_GRAY2RGB)
+        colors = {"ending" : (150, 0, 0), "bifurcation" : (0, 150, 0)}
+
+        # iterate each pixel minutia
+        for i in range(1, x - kernel_size//2):
+            for j in range(1, y - kernel_size//2):
+                minutiae = minutiaExtractor.minutiae_at(biniry_image, j, i, kernel_size)
+                if minutiae != "none":
+                        cv2.circle(result, (i,j), radius=2, color=colors[minutiae], thickness=2)
+            return result
+    
+    @staticmethod
+    def minutiae_at(pixels, i, j, kernel_size):
+        """
+        https://airccj.org/CSCP/vol7/csit76809.pdf pg93
+        Crossing number methods is a really simple way to detect ridge endings and ridge bifurcations.
+        Then the crossing number algorithm will look at 3x3 pixel blocks:
+
+        if middle pixel is black (represents ridge):
+        if pixel on boundary are crossed with the ridge once, then it is a possible ridge ending
+        if pixel on boundary are crossed with the ridge three times, then it is a ridge bifurcation
+
+        :param pixels:
+        :param i:
+        :param j:
+        :return:
+        """
+        # if middle pixel is black (represents ridge)
+        if pixels[i][j] == 1:
+
+            if kernel_size == 3:
+                cells = [(-1, -1), (-1, 0), (-1, 1),        # p1 p2 p3
+                       (0, 1),  (1, 1),  (1, 0),            # p8    p4
+                      (1, -1), (0, -1), (-1, -1)]           # p7 p6 p5
+            else:
+                cells = [(-2, -2), (-2, -1), (-2, 0), (-2, 1), (-2, 2),                 # p1 p2   p3
+                       (-1, 2), (0, 2),  (1, 2),  (2, 2), (2, 1), (2, 0),               # p8      p4
+                      (2, -1), (2, -2), (1, -2), (0, -2), (-1, -2), (-2, -2)]           # p7 p6   p5
+
+            values = [pixels[i + l][j + k] for k, l in cells]
+
+            # count crossing how many times it goes from 0 to 1
+            crossings = 0
+            for k in range(0, len(values)-1):
+                crossings += abs(values[k] - values[k + 1])
+            crossings //= 2
+
+            # if pixel on boundary are crossed with the ridge once, then it is a possible ridge ending
+            # if pixel on boundary are crossed with the ridge three times, then it is a ridge bifurcation
+            if crossings == 1:
+                return "ending"
+            if crossings == 3:
+                return "bifurcation"
+
+        return "none"
     
 
 
 
 fingerprint = minutiaLoader(r"C:\Users\kound\OneDrive\Desktop\finger-50classes\004\L\004_L3_4.bmp")
 
-
+extractor = minutiaExtractor(self.normalised_img,self.segmented_img,self.norm_img,self.mask,self.block)
 plt.figure(figsize=(9, 3))
 
 plt.subplot(1,3,1)
@@ -265,5 +449,5 @@ plt.show()
 plt.tight_layout()
 
 
-
-
+plt.subplot(1,3,3)
+plt.imshow(fingerprint.mask,cmap="gray")
